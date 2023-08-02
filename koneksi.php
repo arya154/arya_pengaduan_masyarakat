4308c29e6179acd0c4b8fdf1d625ae8c0255b682
arya154 <141195736+arya154@users.noreply.github.com>
2023-08-02 10:24:09 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'pengaduan';
+
+    $koneksi = mysqli_connect($hostname,$username,$password,$database);
